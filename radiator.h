73ccfb4004a336e101319e37be0b0a004d150db0
Yujin Han <yuhan@tcd.ie>
2025-04-26 20:06:02 +0100

    refactor (header): streamline radiator.h, only keep CPU interface declaration

--- radiator.h
+++ radiator.h
@@ -3,8 +3,6 @@
 
 #include "precision.h"
 
-void cuda_propagate_heat(FLOAT *matrix, int n, int m, int iterations, float *timings, FLOAT *averages, uint8_t average);
 void cpu_propagate_heat(FLOAT *matrix_a, FLOAT *matrix_b, int n, int m, int iterations, float *timings_cpu, FLOAT *averages, uint8_t average);
-void average_rows(float *matrix, int n, int m, float *averages);
 
 #endif // _RADIATOR_H_
